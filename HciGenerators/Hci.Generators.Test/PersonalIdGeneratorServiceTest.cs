769ba13ec4a83dd85ccbb65ecadcc32010142020
Roman FILIP <dicky@email.cz>
2017-07-03 00:25:08 +0200

    Added project with unit tests

---
+++ HciGenerators/Hci.Generators.Test/PersonalIdGeneratorServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hci.Generators.Web.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hci.Generators.Test
+{
+    [TestClass]
+    public class PersonalIdGeneratorServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var generator = new PersonalIdGeneratorService();
+            generator.GeneratePersonalIds();
+        }
+    }
+}
